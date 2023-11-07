be7e85505e515b930fc53c5d7b99b9db7bf8e898
how2coding <how2coding@gmail.com>
2023-11-07 15:07:55 +0700

    Update Application.java

--- src/main/java/com/dockerforjavadevelopers/hello/Application.java
+++ src/main/java/com/dockerforjavadevelopers/hello/Application.java
@@ -1,6 +1,11 @@
 package com.dockerforjavadevelopers.hello;
 
-
+import java.io.IOException;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ApplicationContext;
@@ -11,10 +16,23 @@ import org.springframework.context.annotation.Configuration;
 @EnableAutoConfiguration
 @ComponentScan
 public class Application {
-    
-    public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+    public static void main(String[] args) throws IOException {
+
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("text/plain");
+        RequestBody body = RequestBody.create(mediaType, "");
+        Request request = new Request.Builder()
+                .url("https://checkip.amazonaws.com/")
+                .get()
+                .build();
+        Response response = client.newCall(request).execute();
         
+        System.out.println(response.body().string());
+
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
     }
 
 }
